33466c5a618a827f89a7eac43adc4c892a70f15d
RoopsagarM <87383232+RoopsagarM@users.noreply.github.com>
2024-06-13 19:18:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,3 +102,8 @@ if ingredients_list:
             st.success('Your Smoothie is ordered, ' + name_on_order, icon="✅")
         except Exception as e:
             st.error(f'Error submitting order: {str(e)}')
+
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
